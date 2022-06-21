123d2f2dd2c33d19acd2f8ca62b33d929f7950c7
shouko <imshouko@gmail.com>
2022-06-22 01:21:02 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jquery: true,
+  },
+  plugins: [
+    'html',
+  ],
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
